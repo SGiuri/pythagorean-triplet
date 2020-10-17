76a17b14112ab61e385737b2517f9b7ce622cbc3
SGiuri <s.giuri@issm.it>
2020-10-17 15:17:12 +0200

    Done

--- pythagorean_triplet.py
+++ pythagorean_triplet.py
@@ -1,10 +1,48 @@
-def triplets_with_sum(number):
-    pass
+from math import gcd
 
+
+def triplets_with_sum(number):
+    primitives_triplets = triplets_in_range(0,number)
+
+    sum_triplets = []
+
+    for triplet in primitives_triplets:
+        if sum(triplet) == number:
+            print("T :", triplet)
+            if triplet not in sum_triplets:
+                sum_triplets.append(triplet)
+        elif number % sum(triplet) == 0:
+            molt = number // sum(triplet)
+            new_triplet = [j * molt for j in triplet]
+            if new_triplet not in sum_triplets:
+                print("NT :", new_triplet)
+                sum_triplets.append(new_triplet)
+
+    return sum_triplets
 
 def triplets_in_range(start, end):
-    pass
+    m = 2
+    n = 1
+    primitives_triplets = []
+    for mm in range(m, end, 1):
+        for nn in range(n, mm, 1):
+            # print(mm, nn)
+            if coprime(mm, nn):
 
+                a = mm ** 2 - nn ** 2
+                b = 2 * mm * nn
+                c = mm ** 2 + nn ** 2
+                somma = a + b + c
+                if sorted([a, b, c]) not in primitives_triplets:
+                    primitives_triplets.append(sorted([a, b, c]))
+            if somma > 2 * end:
+                break
+        if somma > 2 * end:
+            break
+    return primitives_triplets
 
-def is_triplet(triplet):
-    pass
+def coprime(m, n):
+    if gcd(m, n) == 1:
+        return True
+    return False
+
